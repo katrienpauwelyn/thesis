8cc5d167bcecbb02424752401bd62cda349553ab
katrienpauwelyn <katie.pauwelyn@hotmail.com>
2017-01-27 11:58:59 +0100

    leesbaarder maken

--- javacode/ensemblesofND/src/main/java/staticData/Path.java
+++ javacode/ensemblesofND/src/main/java/staticData/Path.java
@@ -11,10 +11,12 @@ package staticData;
  */
 public class Path {
       public static  String path = "/Users/katie/thesisoutput/out";
+      public static String pathDatasets = "/Users/katie/thesiscode/datasets";
       public static  String[] classifiers = {"classBalanced","furthestCentroid","nd","randomPair"};
       public static  String[] datasets = {"audiology","krkopt","letterRecognition","mfeatFac","mfeatFou","mfeatKar","mfeatMor",
         "mfeatPix","optdigits","pageBlocks","pendigits","segmentation","shuttle","vowel","yeast","zoo"};
       public static int nbFolds = 10;
+      public static int nbSeeds = 10;
       
       public static int getIndexOfClassAttribute(String s){
           if(s.equals("letterRecognition")||s.equals("segmentation")){
--- javacode/ensemblesofND/src/main/java/weka/classifiers/meta/nestedDichotomies/GetResults.java
+++ javacode/ensemblesofND/src/main/java/weka/classifiers/meta/nestedDichotomies/GetResults.java
@@ -1,21 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package weka.classifiers.meta.nestedDichotomies;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
 import staticData.Path;
-import weka.classifiers.meta.nestedDichotomies.ClassBalancedND;
-import weka.classifiers.meta.nestedDichotomies.FurthestCentroidND;
-import weka.classifiers.meta.nestedDichotomies.ND;
-import weka.classifiers.meta.nestedDichotomies.RandomPairND;
 
 /**
- *
+ * Runs alle ND programma's op alle train en test files. 
+ * Slaat de resultaten en runtimes op.
  * @author katie
  */
 public class GetResults {
@@ -26,18 +19,18 @@ public class GetResults {
     //dataset: the name of the dataset. 
     //trainPath: the path of the training data
     //testPath: the path of the testing data
-    public String[] getArgs(boolean logistic, String dataset, String trainPath, String testPath){
+    public String[] getArgs(boolean logistic, String dataset, String trainPath, String testPath, String seed){
         if(logistic){
             if(Path.getIndexOfClassAttribute(dataset)<0){
-                return new String[] {"-t", trainPath, "-T", testPath, "-W", logisticRegression};
+                return new String[] {"-t", trainPath, "-T", testPath, "-W", logisticRegression, "-s", seed, "-S", seed};
             } else {
-                return new String[] {"-t", testPath, "-T", trainPath, "-W", logisticRegression, "-c", "1"};
+                return new String[] {"-t", testPath, "-T", trainPath, "-W", logisticRegression, "-c", "1", "-s", seed, "-S", seed};
             }
         } else {
             if(Path.getIndexOfClassAttribute(dataset)<0){
-                return new String[] {"-t", trainPath, "-T", testPath};
+                return new String[] {"-t", trainPath, "-T", testPath, "-s", seed, "-S", seed};
             } else {
-                return new String[] {"-t", trainPath, "-T", testPath, "-c", "1"};
+                return new String[] {"-t", trainPath, "-T", testPath, "-c", "1", "-s", seed, "-S", seed};
             }
         }
     }
@@ -52,106 +45,117 @@ public class GetResults {
     }
     
  
-    
-    public void runAllND(String path) throws FileNotFoundException{
+     public void runAllND(String path) throws FileNotFoundException{
         for(String s: Path.datasets){//for all datasets
-            for(int i = 1; i<11; i++){//for all folds
-                String trainPath = path+"/"+s+"/NDfold"+Integer.toString(i)+".arff";
-                String testPath = path+"/"+s+"/NDtest"+Integer.toString(i)+".arff"; 
-                PrintStream outTrue = new PrintStream(new FileOutputStream(path+"/"+s+"NDoutputLogistic"+Integer.toString(i)));
-                System.setOut(outTrue);
-                
-                ND.main(getArgs(true, s, trainPath, testPath));
-                outTrue.close();
-               
-                PrintStream outFalse = new PrintStream(new FileOutputStream(path+"/"+s+"NDoutputC45"+Integer.toString(i)));
-                System.setOut(outFalse);
-               
-                ND.main(getArgs(false, s, trainPath, testPath));
-                outFalse.close();
+            PrintStream timeStream = new PrintStream(new File(
+            path+"/nd/"+s+"/aOutputND/NDaTime.txt"));
+            for(int seed= 0; seed<Path.nbSeeds; seed++){//for all seeds
+                for(int i = 1; i<11; i++){//for all folds
+                   String trainPath = path+"/nd/"+s+"/NDS"+seed+"fold"+Integer.toString(i)+".arff";
+                   String testPath = path+"/nd/"+s+"/NDS"+seed+"test"+Integer.toString(i)+".arff";
+                  
+                   PrintStream outStream = new PrintStream(new FileOutputStream(
+                           path+"/nd/"+s+"/aOutputND/NDS"+seed+"outputC45"+Integer.toString(i)));
+                   System.setOut(outStream);
+                   
+                   long startTime = System.nanoTime();
+                   ND.main(getArgs(false, s, trainPath, testPath, Integer.toString(seed)));
+                   long endTime = System.nanoTime();
+                   long durationMiliSeconds = (endTime - startTime)/1000000;
+                   timeStream.println(durationMiliSeconds);
+                   
+                   outStream.close();
+                }
             }
-        }
-        
-    
-        
-    }
-    public void runRandomPair(String path, String dataset) throws FileNotFoundException{
-          for(int i = 1; i<11; i++){
-                String trainPath = path+"/NDfold"+Integer.toString(i)+".arff";
-                String testPath = path+"/NDtest"+Integer.toString(i)+".arff"; 
-                PrintStream outTrue = new PrintStream(new FileOutputStream(path+"NDoutputLogistic"+Integer.toString(i)));
-               System.setOut(outTrue);
-                RandomPairND.main(getArgs(true, dataset, trainPath, testPath));
-                outTrue.close();
-               
-                 PrintStream outFalse = new PrintStream(new FileOutputStream(path+"NDoutputC45"+Integer.toString(i)));
-                System.setOut(outFalse);
-                RandomPairND.main(getArgs(false, dataset, trainPath, testPath));
-                outFalse.close();
-            }
+            timeStream.close();
+        }     
     }
     
     public void runAllRandomPair(String path) throws FileNotFoundException{
-        for(String s: Path.datasets){
-            
-          runRandomPair(path+"/"+s, s);
+        for(String s: Path.datasets){//for all datasets
+            PrintStream timeStream = new PrintStream(new File(
+               path+"/randomPair/"+s+"/aOutputND/NDaTime.txt"));
+
+           for(int seed = 0; seed<Path.nbSeeds; seed++){
+               for(int i = 1; i<11; i++){//for all folds
+                   String trainPath = path+"/randomPair/"+s+"/NDS"+seed+"fold"+Integer.toString(i)+".arff";
+                   String testPath = path+"/randomPair/"+s+"/NDS"+seed+"test"+Integer.toString(i)+".arff"; 
+
+                   PrintStream outStream = new PrintStream(new FileOutputStream(path+"/randomPair/"+s+
+                           "/aOutputND/NDS"+seed+"outputC45"+Integer.toString(i)));
+                   System.setOut(outStream);
+                   
+                   long startTime = System.nanoTime();
+                   RandomPairND.main(getArgs(false, s, trainPath, testPath, Integer.toString(seed)));
+                   long endTime = System.nanoTime();
+                   long durationMiliSeconds = (endTime - startTime)/1000000;
+                   timeStream.println(durationMiliSeconds);
+                   
+                   outStream.close();
+                }
+            }
+           timeStream.close();
         }
     }
     
+    
     public void runAllFurthestCentroid(String path) throws FileNotFoundException{
-        for(String s: Path.datasets){
-            for(int i = 1; i<11; i++){
-                String trainPath = path+"/"+s+"/NDfold"+Integer.toString(i)+".arff";
-                String testPath = path+"/"+s+"/NDtest"+Integer.toString(i)+".arff"; 
-                PrintStream outTrue = new PrintStream(new FileOutputStream(path+"/"+s+"NDoutputLogistic"+Integer.toString(i)));
-                System.setOut(outTrue);
-                FurthestCentroidND.main(getArgs(true, s, trainPath, testPath));
-                outTrue.close();
-                
-                PrintStream outFalse = new PrintStream(new FileOutputStream(path+"/"+s+"NDoutputC45"+Integer.toString(i)));
-                System.setOut(outFalse);
-                FurthestCentroidND.main(getArgs(false, s, trainPath, testPath));
-                outFalse.close();                
+        for(String s: Path.datasets){//for all datasets
+             PrintStream timeStream = new PrintStream(new File(
+               path+"/furthestCentroid/"+s+"/aOutputND/NDaTime.txt"));
+
+            for(int seed= 0; seed<Path.nbSeeds; seed++){//for all seeds
+                  for(int i = 1; i<11; i++){//for all folds
+                        String trainPath = path+"/furthestCentroid/"+s+"/NDS"+seed+"fold"+Integer.toString(i)+".arff";
+                        String testPath = path+"/furthestCentroid/"+s+"/NDS"+seed+"test"+Integer.toString(i)+".arff"; 
+                   
+                        PrintStream outStream = new PrintStream(
+                                new FileOutputStream(path+"/furthestCentroid/"+s+"/aOutputND/NDS"+seed+"outputC45"+Integer.toString(i)));
+                        System.setOut(outStream);
+                         long startTime = System.nanoTime();
+                        FurthestCentroidND.main(getArgs(false, s, trainPath, testPath, Integer.toString(seed)));
+                        long endTime = System.nanoTime();
+                          long durationMiliSeconds = (endTime - startTime)/1000000;
+                        timeStream.println(durationMiliSeconds);
+                   
+                        outStream.close();                
             }
+            }
+          timeStream.close();
         }
     }
     
     
     
     public void runAllClassBalanced(String path) throws FileNotFoundException{
-        for(String s: Path.datasets){
-            for(int i = 1; i<11; i++){
-                String trainPath = path+"/"+s+"/NDfold"+Integer.toString(i)+".arff";
-                String testPath = path+"/"+s+"/NDtest"+Integer.toString(i)+".arff"; 
-                PrintStream outTrue = new PrintStream(new FileOutputStream(path+"/"+s+"NDoutputLogistic"+Integer.toString(i)));
-                System.setOut(outTrue);
-                ClassBalancedND.main(getArgs(true, s, trainPath, testPath));
-                outTrue.close();
+        for(String s: Path.datasets){//for all datasets
+             PrintStream timeStream = new PrintStream(new File(
+               path+"/classBalanced/"+s+"/aOutputND/NDaTime.txt"));
+
+            for(int seed = 0; seed < Path.nbSeeds; seed++){
+                for(int i = 1; i<11; i++){
+                    String trainPath = path+"/classBalanced/"+s+"/NDS"+seed+"fold"+Integer.toString(i)+".arff";
+                    String testPath = path+"/classBalanced/"+s+"/NDS"+seed+"test"+Integer.toString(i)+".arff"; 
                 
-                PrintStream outFalse = new PrintStream(new FileOutputStream(path+"/"+s+"NDoutputC45"+Integer.toString(i)));
-                System.setOut(outFalse);
-                ClassBalancedND.main(getArgs(false, s, trainPath, testPath));
-                outFalse.close();
+                    PrintStream outStream = new PrintStream(
+                            new FileOutputStream(path+"/classBalanced/"+s+"/aOutputND/NDS"+seed+"outputC45"+Integer.toString(i)));
+                    System.setOut(outStream);
+                     long startTime = System.nanoTime();
+                    ClassBalancedND.main(getArgs(false, s, trainPath, testPath, Integer.toString(seed)));
+                     long endTime = System.nanoTime();
+                          long durationMiliSeconds = (endTime - startTime)/1000000;
+                        timeStream.println(durationMiliSeconds);
+                   
+                    outStream.close();
             }
+            }
+            timeStream.close();
         }
     }
-    
-    
-    
-    
-    
+   
     public static void main(String[] args) throws FileNotFoundException{
         GetResults getIt = new GetResults();
-        String basicPath = Path.path;
-        
-       //getIt.runAllND(basicPath+"/nd");
-
-       int size = Path.datasets.length;
-    //   getIt.runRandomPair(basicPath+"/randomPair/"+Path.datasets[size-2], Path.datasets[size-2]);
-      //  getIt.runAllRandomPair(basicPath+"/randomPair");
-       
-     //  getIt.runAllClassBalanced(basicPath+"/classBalanced");
-     //  getIt.runAllFurthestCentroid(basicPath+"/furthestCentroid");
+        getIt.runAllClassBalanced(Path.path);
         
     }
     
